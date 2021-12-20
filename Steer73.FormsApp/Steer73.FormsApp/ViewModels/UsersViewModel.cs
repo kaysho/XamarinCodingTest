0b6e5ef86432102da12fa7d0ed84935e771c3183
Damilola Shopeyin <kaysho109@gmail.com>
2021-12-20 10:37:22 +0100

    Add sorting and delete functionality.

--- Steer73.FormsApp/Steer73.FormsApp/ViewModels/UsersViewModel.cs
+++ Steer73.FormsApp/Steer73.FormsApp/ViewModels/UsersViewModel.cs
@@ -2,7 +2,10 @@
 using Steer73.FormsApp.Model;
 using System;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace Steer73.FormsApp.ViewModels
 {
@@ -10,12 +13,30 @@ namespace Steer73.FormsApp.ViewModels
     {
         private readonly IUserService _userService;
         private readonly IMessageService _messageService;
+        public ICommand DeleteCommand { get; }
 
         public UsersViewModel(IUserService userService,
                               IMessageService messageService)
         {
             _userService = userService;
             _messageService = messageService;
+            DeleteCommand = new Command(DeleteFirstUser);
+        }
+
+        private void DeleteFirstUser()
+        {
+            try
+            {
+                if (Users != null && Users.Count != 0)
+                {
+                    Users.RemoveAt(0);
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+
         }
 
         public async Task Initialize()
@@ -29,6 +50,12 @@ namespace Steer73.FormsApp.ViewModels
                 foreach (var user in users)
                 {
                     Users.Add(user);
+                }
+
+                if (Users != null && Users.Count != 0)
+                {
+                    var orderedUsers = Users.OrderBy(x => x.FullName);
+                    Users = new ObservableCollection<User>(orderedUsers);
                 }
             }
             catch (Exception ex)
@@ -48,7 +75,11 @@ namespace Steer73.FormsApp.ViewModels
             set { SetProperty(ref isBusy, value); }
         }
 
-
-        public ObservableCollection<User> Users { get; } = new ObservableCollection<User>();
+        ObservableCollection<User> users = new ObservableCollection<User>();
+        public ObservableCollection<User> Users
+        {
+            get { return users; }
+            set { SetProperty(ref users, value); }
+        }
     }
 }
